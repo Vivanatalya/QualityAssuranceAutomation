f16546ecc6ab56a3785b01282ac507c415de4069
Viva Natalya <vivanatalya@gmail.com>
2020-12-20 23:42:26 +0200

    add new project

---
+++ NROMANCHUKATQA-2020/src/main/java/com/atqa2020nromanchuk/App.java
@@ -0,0 +1,13 @@
+package com.atqa2020nromanchuk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
