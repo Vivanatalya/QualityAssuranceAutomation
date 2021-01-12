5fe33acfe4794e6a744c51f7a1f873860d8ce68b
Oleksandr Perepelytsya <o.perepelytsya@gmail.com>
2021-01-12 15:12:43 +0000

    Update MainMenu.java

--- NROMANCHUKATQA-2020/src/main/java/com/atqa2020nromanchuk/framework/MainMenu.java
+++ NROMANCHUKATQA-2020/src/main/java/com/atqa2020nromanchuk/framework/MainMenu.java
@@ -6,6 +6,7 @@ import com.atqa2020nromanchuk.pages.Page;
 
 public class MainMenu {
 
+    //TODO please apply formatting to the page, e.g. remove redundant empty lines, add new lines where expected, and align methods
     private String productsLowerTwentyButtonLocator = "//body/aside/div[2]/nav[2]/ul/li[3]/a";
     protected RemoteWebDriver driver;
     private String newLowerPricesLinkLocator = "//body/aside/div[2]/nav[@class='hnf-menu__nav2']/ul/li[2]/a";
