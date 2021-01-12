a1fcb1fd632639c59b2a4c5796846370727df394
Oleksandr Perepelytsya <o.perepelytsya@gmail.com>
2021-01-12 15:15:26 +0000

    Update KitchenPage.java

--- NROMANCHUKATQA-2020/src/main/java/com/atqa2020nromanchuk/pages/KitchenPage.java
+++ NROMANCHUKATQA-2020/src/main/java/com/atqa2020nromanchuk/pages/KitchenPage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import com.atqa2020nromanchuk.pages.Page;
 
 public class KitchenPage extends Page {
-
+    //TODO you can use single quotes in the locator, e.g. "//*[@id='ea03b70f-28d1-11eb-886e-1db5641597a2']/div/div[2]/div/div/a/span";
     private String galleryButtonLocator="//*[@id=\"ea03b70f-28d1-11eb-886e-1db5641597a2\"]/div/div[2]/div/div/a/span";
 
     public KitchenPage(RemoteWebDriver driver) {
